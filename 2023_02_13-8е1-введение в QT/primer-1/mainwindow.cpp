1dfbc15127f9e44819af2c3ffe21fc4aa00694b6
Alexander Lobachev <ss_di@mail.ru>
2023-02-13 11:00:02 +0300

    8е1 - введение в QT

---
+++ 2023_02_13-8е1-введение в QT/primer-1/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnRun_clicked()
+{
+    ui->lblOut->setText(ui->edtInput->text());
+}
