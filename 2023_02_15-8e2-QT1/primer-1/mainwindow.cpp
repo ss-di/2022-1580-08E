9acc17c19cd66ddcfc11dbb6d9328caf4c55e1a7
Alexander Lobachev <ss_di@mail.ru>
2023-02-15 11:20:04 +0300

    8е2 знакомство с QT

---
+++ 2023_02_15-8e2-QT1/primer-1/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnRun_clicked()
+{
+    ui->lblOut->setText(ui->edtInput->text());
+
+}
