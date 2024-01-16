64d3113950ebc29e8aa37b514ebb034c4ea31b78
Alexander Lobachev <ss_di@mail.ru>
2024-01-16 11:14:36 +0300

    dfs

---
+++ 2024_01_16 - DFS/main.cpp
@@ -0,0 +1,15 @@
+
+void dfs(const vector<vector<int>> &mat, vector<int> &p, int t, int f) {
+    p[t] = f;
+    for (int i = 0; i < int(mat[t].size()); i+= 1)
+        if (mat[t][i] == 1 && p[i] != -2)
+            dfs(mat, p, i, t);
+}
+
+int main()
+{
+
+    dfs(mat, p, 0, -1);
+
+    return 0;
+}
